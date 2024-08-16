342b3888ca63f893fa331933c8631c9dc32dabeb
Hamza El Omrani <hel-omra@e3r3p13.1337.ma>
2024-08-16 18:02:09 +0100

    /latest update/

---
+++ philo_bonus/actions_bonus.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   actions_bonus.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hel-omra <hel-omra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/16 00:04:36 by hel-omra          #+#    #+#             */
+/*   Updated: 2024/08/16 18:01:14 by hel-omra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo_bonus.h"
+
+void	eating(t_philo *philo)
+{
+	pthread_mutex_lock(&philo->data->fork[philo->l_fork]);
+	print(philo, TAKING);
+	pthread_mutex_lock(&philo->data->fork[philo->r_fork]);
+	print(philo, TAKING);
+	pthread_mutex_lock(&philo->data->var);
+	philo->last_time_eat = get_time();
+	philo->meals_count++;
+	pthread_mutex_unlock(&philo->data->var);
+	print(philo, EATING);
+	ft_usleep(philo->data->time_to_eat, philo);
+	pthread_mutex_unlock(&philo->data->fork[philo->r_fork]);
+	pthread_mutex_unlock(&philo->data->fork[philo->l_fork]);
+}
+
+void	print(t_philo *philo, char *action)
+{
+	pthread_mutex_lock(&philo->data->die);
+	if (philo->data->sm1_died == TRUE)
+	{
+		pthread_mutex_unlock(&philo->data->die);
+		return ;
+	}
+	printf("%ld\t %d %s\n", get_time() - philo->data->start, philo->id, action);
+	pthread_mutex_unlock(&philo->data->die);
+}
+
+void	sleeping(t_philo *philo)
+{
+	print(philo, SLEEPING);
+	ft_usleep(philo->data->time_to_sleep, philo);
+}
